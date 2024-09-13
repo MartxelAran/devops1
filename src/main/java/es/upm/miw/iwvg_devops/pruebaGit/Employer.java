31e915afdc67513886a35b3dbafaf3ed7542bb75
MartxelAranzadi <martxel.aranzadi@gmail.com>
2024-09-13 19:45:11 +0200

    "Crear nueva clase y modificarla"

---
+++ src/main/java/es/upm/miw/iwvg_devops/pruebaGit/Employer.java
@@ -0,0 +1,12 @@
+package es.upm.miw.iwvg_devops.pruebaGit;
+
+public class Employer {
+    private String name;
+    private String lastName;
+    private int age;
+    public Employer(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
