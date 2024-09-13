90213cfc5852528f9e69c2bfec7a2585123d43f0
MartxelAranzadi <martxel.aranzadi@gmail.com>
2024-09-13 19:41:51 +0200

    "Modificar archivo cliente"

--- src/main/java/es/upm/miw/iwvg_devops/pruebaGit/Client.java
+++ src/main/java/es/upm/miw/iwvg_devops/pruebaGit/Client.java
@@ -1,4 +1,48 @@
 package es.upm.miw.iwvg_devops.pruebaGit;
 
 public class Client {
+    public int id;
+    public String name;
+    public String lastName;
+    public String password;
+    public String email;
+
+    public Client(int id, String name, String lastName, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
