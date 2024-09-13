5d63ea1a86c1271c4d137a6f7475f3b8d5aaad43
MartxelAranzadi <martxel.aranzadi@gmail.com>
2024-09-13 20:30:01 +0200

    "Add getters and setters"

--- src/main/java/es/upm/miw/iwvg_devops/pruebaGit/Employer.java
+++ src/main/java/es/upm/miw/iwvg_devops/pruebaGit/Employer.java
@@ -9,4 +9,28 @@ public class Employer {
         this.lastName = lastName;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
